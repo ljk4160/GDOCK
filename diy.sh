d384b943becf22b7330b34494ff11fc17dc3fb2d
JK <405901422@qq.com>
2019-12-02 00:31:49 +0800

    个性化固件第一版
    1增加自定义脚本
    2增加agron主题

--- diy.sh
+++ diy.sh
@@ -1,1 +1,15 @@
-sed -i 's/Openwrt/G-DOCK/g' package/base-files/files/bin/config_generate
+echo '修改机器名称'
+sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+echo '修改默认主题'
+sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+
+echo '去除默认bootstrap主题'
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+echo '删除旧版argon'
+rm -rf ./package/lean/luci-theme-argon
+ln -s ../../../luci-theme-argon ./package/lean/
