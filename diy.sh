7f12a7fc7a1742ced788f4fd97b1353010753fbb
tokyai <121705033@qq.com>
2019-12-03 19:53:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,7 +2,7 @@ echo '修改机器名称'
 sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
 
 #echo '修改网关地址'
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
