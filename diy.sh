d52559d0d973e7bdb7a580ecf22fe830f3eed0e2
diyuanabc <67855343+diyuanabc@users.noreply.github.com>
2020-07-06 17:03:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -18,7 +18,7 @@ rm -rf ./package/lean/luci-theme-argon
 ln -s ../../../luci-theme-argon ./package/lean/
 
 echo '修改wifi名称'
-sed -i 's/OpenWrt/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/OpenWrt/HUI/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 echo '修改banner'
 rm -rf package/base-files/files/etc/banner
